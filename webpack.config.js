e3e42f58925a651e548e093dab903a63d2c59572
EkaterinaZ <KateLegion@yandex.ru>
2022-10-20 12:51:37 +0300

    1

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: { main: './src/components/index.js' },
+  entry: { main: './src/pages/index.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
@@ -34,9 +34,9 @@ module.exports = {
           loader: 'css-loader',
           options: { importLoaders: 1 }
         },
-          'postcss-loader']
-      },
-
+          'postcss-loader'
+        ]
+      }
     ]
   },
   plugins: [
